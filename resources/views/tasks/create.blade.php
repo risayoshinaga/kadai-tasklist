017a4c06a9124687c4d449920865a14f71494df6
taro.kirameki <taro.kirameki@techacademy.jp>
2018-06-04 04:29:50 +0000

    changed one part

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 
-    <h1>タスク新規作成ページ</h1>
+   
     
-    <div class="row">
+    <div class="row"></div>
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+             <h1>タスク新規作成ページ</h1>
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
         <div class="form-group">
