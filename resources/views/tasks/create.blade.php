11f05ee51e119a605d5776f96267e4c0557e8f11
taro.kirameki <taro.kirameki@techacademy.jp>
2018-06-04 04:34:39 +0000

    changed a bit

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,7 @@
    
     
     <div class="row"></div>
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
              <h1>タスク新規作成ページ</h1>
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
@@ -21,7 +21,6 @@
                 
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
        
-
     {!! Form::close() !!}
 
 @endsection
